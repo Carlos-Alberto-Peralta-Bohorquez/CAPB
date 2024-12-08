450dcdbfdc28b8100e5179ac18031aa8103fe4db
Carlos-Alberto-Peralta-Bohorquez <canderobe@outlook.com>
2024-12-08 00:06:30 -0600

    Add files via upload

---
+++ 04 INTERFAZ IncidenciasObra/CRUD-MVC-JAVA/src/java/ModeloDAO/PersonaDAO.java
@@ -0,0 +1,100 @@
+
+package ModeloDAO;
+
+import Config.Conexion;
+import Intefaces.CRUD;
+import Modelo.Persona;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonaDAO implements CRUD{
+    Conexion cn=new Conexion();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    Persona p=new Persona();
+    
+    @Override
+    public List listar() {
+        ArrayList<Persona>list=new ArrayList<>();
+        String sql="select * from persona";
+        try {
+            con=cn.getConnection();
+            ps=con.prepareStatement(sql);
+            rs=ps.executeQuery();
+            while(rs.next()){
+                Persona per=new Persona();
+                per.setId(rs.getInt("Id"));
+                per.setDni(rs.getString("FechaReporte"));
+                per.setNom(rs.getString("NombreObra"));
+                per.setHab(rs.getString("NombresPersonasAsunto"));
+                per.setMot(rs.getString("Motivo"));
+                list.add(per);
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    @Override
+    public Persona list(int id) {
+        String sql="select * from persona where Id="+id;
+        try {
+            con=cn.getConnection();
+            ps=con.prepareStatement(sql);
+            rs=ps.executeQuery();
+            while(rs.next()){                
+                p.setId(rs.getInt("Id"));
+                p.setDni(rs.getString("FechaReporte"));
+                p.setNom(rs.getString("NombreObra"));
+                p.setHab(rs.getString("NombresPersonasAsunto"));
+                 p.setMot(rs.getString("Motivo"));
+                
+            }
+        } catch (Exception e) {
+        }
+        return p;
+    }
+
+    @Override
+    public boolean add(Persona per) {
+       String sql="insert into persona(FechaReporte, NombreObra,NombresPersonasAsunto,Motivo)values('"+per.getDni()+"','"+per.getNom()+"','"+per.getHab()+"','"+per.getMot()+"')";
+                                                                                
+       try {
+            con=cn.getConnection();
+            ps=con.prepareStatement(sql);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+       return false;
+    }
+
+    @Override
+    public boolean edit(Persona per) {
+        String sql="update persona set FechaReporte='"+per.getDni()+"',NombreObra='"+per.getNom()+"',NombresPersonasAsunto='"+per.getHab()+"',Motivo='"+per.getMot()+"'where Id="+per.getId();
+                                                             
+        try {
+            con=cn.getConnection();
+            ps=con.prepareStatement(sql);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+        return false;
+    }
+
+    @Override
+    public boolean eliminar(int id) {
+        String sql="delete from persona where Id="+id;
+        try {
+            con=cn.getConnection();
+            ps=con.prepareStatement(sql);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+        return false;
+    }
+    
+}
